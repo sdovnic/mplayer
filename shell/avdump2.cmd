e812555b1651e1f31143179e69d218d9b5d809bc
sdovnic <sdovnic@web.de>
2013-01-02 05:38:21 +0100

    Save AniDB Credentials in ext. INI File

--- shell/avdump2.cmd
+++ shell/avdump2.cmd
@@ -1,7 +1,13 @@
 @echo off
 set mplayer=F:\mplayer
-set login="--Auth=user:pass"
 set ed2k="--Exp=ed2k.txt"
 set done="--Done=done.txt"
+rem rename av2dump.ini.contrib to av2dump.ini
+rem enter your anidb credentials into the the file
+for /f "tokens=1,2 delims==" %%a in (%mplayer%\shell\avdump2.ini) do (
+	if %%a==user set user=%%b
+	if %%a==pass set pass=%%b
+	set login="--Auth=%user%:%pass%"
+)
 "%mplayer%\avdump2\AVDump2CL.exe" %login% --OpenAniDBLink %done% %ed2k% %1
 pause
