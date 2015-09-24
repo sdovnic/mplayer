635700db2bb1650f28326f82796f7685e8180e90
sdovnic <sdovnic@web.de>
2015-09-24 03:04:07 +0200

    Added other Builds and 32-Bit Download, updated jQuery

--- js/mplayer.js
+++ js/mplayer.js
@@ -1,19 +1,21 @@
 jQuery(document).ready(function($) {
-	$("a").click(function(event) {
-		event.preventDefault();
-		var windowheight = $(window).height();
-		$("#splash").css({
-			"position": "relative",
-			"height": windowheight + "px"
+	$("a.download").each(function() {
+		$(this).click(function(event) {
+			event.preventDefault();
+			var windowheight = $(window).height();
+			$("#splash").css({
+				"position": "relative",
+				"height": windowheight + "px"
+			});
+			$("#download").css({
+				"position": "relative",
+				"min-height": windowheight + "px"
+			}).show(0, function() {
+				$("body, html").animate({
+					scrollTop: windowheight
+				}, 600);
+			});
+			window.location = $(this).attr("href");
 		});
-		$("#download").css({
-			"position": "relative",
-			"min-height": windowheight + "px"
-		}).show(0, function() {
-			$("body, html").animate({
-				scrollTop: windowheight
-			}, 600);
-		});
-		window.location = $("a").attr("href");
 	});
 });
