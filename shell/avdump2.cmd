62b94756f9d58a4b6170250c385f8f44a809f349
Svenja Dovnic <sdovnic@web.de>
2015-09-25 10:00:47 +0200

    Fixed some issues while merging.

--- shell/avdump2.cmd
+++ shell/avdump2.cmd
@@ -1,8 +1,5 @@
 @echo off
-<<<<<<< HEAD
-=======
 setlocal
->>>>>>> origin/master
 set mplayer=C:\Portable\mplayer
 set ed2k="--Exp=ed2k.txt"
 set done="--Done=done.txt"
@@ -11,12 +8,6 @@ rem Enter your AniDB Credentials into the the File
 for /f "tokens=1,2 delims==" %%a in (%mplayer%\shell\avdump2.ini) do (
 	if %%a==user set user=%%b
 	if %%a==pass set pass=%%b
-<<<<<<< HEAD
-	set login="--Auth=%user%:%pass%"
-)
-"%mplayer%\avdump\AVDump2CL.exe" %login% --OpenAniDBLink %done% %ed2k% "%1"
-pause
-=======
 )
 if exist %user% do (
 	set login="--Auth=%user%:%pass%"
@@ -24,4 +15,3 @@ if exist %user% do (
 "%mplayer%\avdump2\AVDump2CL.exe" %login% --OpenAniDBLink %done% %ed2k% "%1"
 pause
 endlocal
->>>>>>> origin/master
